c0f7864923732218ecb5984f26e11deae48ea1b4
kedar <kedareshwar72381@gmail.com>
2024-11-01 12:01:58 +0530

    Refactor book search functionality

--- Library Management/SearchBook.java
+++ Library Management/SearchBook.java
@@ -2,26 +2,31 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SearchBook {
     public static void searchBooks() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("lib_data.txt"));
-        System.out.println("Enter Book ID:");
+        System.out.println("Search for book (ID/Title/Author/Genre): ");
         Scanner inpObj = new Scanner(System.in);
-        String bookID = inpObj.next();
+        String searchTerm = inpObj.next();
+        Pattern pattern = Pattern.compile(searchTerm, Pattern.CASE_INSENSITIVE);
         String line;
-            while ((line = reader.readLine()) != null) {
+
+        while ((line = reader.readLine()) != null) {
+            Matcher matcher = pattern.matcher(line);
+            if (matcher.find()) {
                 String[] bookDetails = line.split(", ");
-                if (bookDetails[0].equals(bookID)) {
-                    System.out.println("Book ID: " + bookDetails[0]);
-                    System.out.println("Title: " + bookDetails[1]);
-                    System.out.println("Author: " + bookDetails[2]);
-                    System.out.println("Genre: " + bookDetails[3]);
-                    System.out.println("Availability: " + bookDetails[4]);
-                    break;
-                }
+                System.out.println("\n------------------------------\n");
+                System.out.println("Book ID: " + bookDetails[0]);
+                System.out.println("Title: " + bookDetails[1]);
+                System.out.println("Author: " + bookDetails[2]);
+                System.out.println("Genre: " + bookDetails[3]);
+                System.out.println("Availability: " + bookDetails[4]);
             }
-            reader.close();
+        }
+    reader.close();
 
     }
 }
